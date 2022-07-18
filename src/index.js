60f45e8859bf5a84a7b0256c81c0d86634a4a924
Eddie Lam <eddie.cm.lam@gtomato.com>
2022-07-18 12:42:34 +0800

    testing lifecycle events

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@ const renderPlaces = require("./utils/renderPlaces");
 window.onload = () => {
   let places = staticLoadPlaces();
   renderPlaces(places);
+
+  const infoBox = document.querySelector("div.info-box");
+
+  const appendInfoBoxMsg = function (info) {
+    infoBox.innerHTML = JSON.stringify(info, null, 2);
+  };
+
+  appendInfoBoxMsg("Loading entity places ...");
+
+  document.addEventListener("gps-entity-place-loaded", function () {
+    appendInfoBoxMsg("Entity places LOADED!");
+  });
+
+  document.addEventListener("gps-entity-place-added", function () {
+    appendInfoBoxMsg("Entity places ADDED!");
+  });
+
+  document.addEventListener("gps-camera-update-position", function (payload) {
+    appendInfoBoxMsg(payload);
+  });
 };
